871c291600fcadca2255899473cab90c9762f97e
ish10 <ishpreetsingh923@gmail.com>
2021-09-03 16:56:02 -0400

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const slider_content = document.querySelector('#box');
 const nextbutton=  document.querySelector(".nxtbtn");
 const previousbutton = document.querySelector(".prevbtn");
 const selectedradio = Array.from(document.querySelectorAll("input"));
+const radiobtn1 = document.querySelector("#r1");
+const radiobtn2 = document.querySelector("#r2");
+const radiobtn3 = document.querySelector("#r3");
 
 const imagearray =Array.from(document.querySelectorAll("#box img")); 
 const playpausebutton = document.querySelector("#play_pause");
@@ -75,3 +78,21 @@ const myvar = setInterval(()=>{
       slider_content.innerHTML = "<img src="+imagearray[i].src+">";
       selectedradio[i].checked=true;
 } , 4000);
+radiobtn1.addEventListener('click',()=>{
+    slider_content.innerHTML = "<img src="+imagearray[0].src+">";
+    selectedradio[0].checked=true;
+
+    
+})
+radiobtn2.addEventListener('click',()=>{
+    slider_content.innerHTML = "<img src="+imagearray[1].src+">";
+    selectedradio[1].checked=true;
+
+    
+})
+radiobtn3.addEventListener('click',()=>{
+    slider_content.innerHTML = "<img src="+imagearray[2].src+">";
+    selectedradio[2].checked=true;
+
+    
+})
